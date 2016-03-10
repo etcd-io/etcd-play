3df0e28216987821c2d6444a952619358626631e
Gyu-Ho Lee <gyuhox@gmail.com>
2016-03-10 13:50:08 -0800

    add docker ps at the end of script

--- etcd_play_web.sh
+++ etcd_play_web.sh
@@ -37,3 +37,5 @@ mkdir -p $HOME/logs
 sudo docker pull quay.io/coreos/etcd-play:latest
 AGENT_RPC_ENDPOINTS='10.128.0.2:9027,10.128.0.3:9027,10.128.0.4:9027,10.128.0.5:9027,10.128.0.6:9027'
 nohup sudo docker run --net=host -p 8000:8000 quay.io/coreos/etcd-play:latest /go/bin/etcd-play web --port :8000 --keep-alive --linux-auto-port=false --production --remote --agent-endpoints="$(echo $AGENT_RPC_ENDPOINTS)" > $HOME/logs/play.log 2>&1 &
+
+sudo docker ps
